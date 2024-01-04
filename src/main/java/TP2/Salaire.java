14ccbff45a578885dc116806b091d83fb496bee8
Deyborah <deyborah.roger@gmail.com>
2024-01-04 14:21:55 +0100

    Salaire et SalaireTest

--- src/main/java/TP2/Salaire.java
+++ src/main/java/TP2/Salaire.java
@@ -1,9 +1,10 @@
 package TP2;
 
+
 public class Salaire {
     public double TauxHoraire= 9.22;
     public double tauxMajore=11.53;
-    public double payer(int heuresTravaille){
+    public double payer(double heuresTravaille){
         return calculSalaire(heuresTravaille);
     }
 
@@ -12,10 +13,12 @@ public class Salaire {
      * @param heuresTravaille les heures de travail effectué
      * @return le salaire
      */
-    private double calculSalaire(int heuresTravaille){
+    private double calculSalaire(double heuresTravaille){
         //si le salarié a fait 151 heures 67 ou moins on multiplie par le taux horaire standard
         if(heuresTravaille<=151.67) {
-            return heuresTravaille * this.TauxHoraire;
+            double salaire=heuresTravaille * this.TauxHoraire;
+            double salaireFinal = (double) Math.round(salaire * 100) / 100;
+            return  salaireFinal;
         }
 
         /*sinon on calcul les heures supplémentaires et on les multiplie par le taux horaire majoré de 25%
@@ -24,7 +27,12 @@ public class Salaire {
         else {
             double heuresSup = heuresTravaille - 151.67;
             double salaireHsup = heuresSup * this.tauxMajore;
-            return (heuresTravaille*this.TauxHoraire)+salaireHsup;
+            double salaireInitiale= 151.67*this.TauxHoraire;
+            double salaireBase= salaireInitiale+salaireHsup;
+            double salaireFinal = (double) Math.round(salaireBase * 100) / 100;
+            return salaireFinal;
+
+
         }
     }
 }
--- src/test/java/TP2/SalaireTest.java
+++ src/test/java/TP2/SalaireTest.java
@@ -1,5 +1,40 @@
 package TP2;
 
+import TP1.exercice1Moyenne;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SalaireTest {
+    private Salaire payer;
+    private double heuresTravaille;
+
+    @BeforeEach
+    public void setUp() {
+
+        payer = new Salaire();
+    }
+
+    @Test
+    public void Test151heures(){
+        double heuresTravaille=151.67;
+        double result = payer.payer(heuresTravaille);
+        assertEquals(1398.40, result);
+    }
+
+
+    @Test
+    public void Test172heures(){
+        double heuresTravaille=172;
+        double result = payer.payer(heuresTravaille);
+        assertEquals(1632.80, result);
+    }
+
 
 }
+
+
+
