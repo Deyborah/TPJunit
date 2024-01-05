84f6237336ac1d980a96b5e78cbafbd4ec6643f4
Deyborah <deyborah.roger@gmail.com>
2024-01-05 14:55:44 +0100

    test1-temperature-echec

---
+++ src/test/java/TP3/TemperatureTest.java
@@ -0,0 +1,15 @@
+package TP3;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class TemperatureTest {
+    @Test
+    public void temperature_proche_de_0() {
+        // Given
+        Temperature temperature = new Temperature();
+        // When
+        String result = temperature.say(0);
+        // Then
+        assertEquals("0", result);
+    }
+}
